f11c93484661f43096ad9bc0715bf1388a643297
Rigaud <BSRigaud@mdanderson.org>
2021-09-24 11:38:06 -0500

    change readme and test on radiotherapy dicom with RTstruct

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Dicom_Reporter_Class
+
+This class is a DICOM listener that will create a report of all the dicom seriesID/patientID in the output directory as well as possibibly convert them to niftii images.
+
+### Example
+
+```
+from Dicom_Reporter_Class import *
+
+'''
+:param input_dir:
+:param output_dir:
+:param force_rewrite:
+:param save_json:
+:param load_json:
+:param supp_tags:
+:param nb_threads:
+:param verbose:
+'''
+
+def main():
+    supp_tags = {
+        'MammoDesc': '0055|1001'
+    }
+
+    time_start = time.time()
+    dicom_explorer = Dicom_Reporter(input_dir=input_dir,
+                                    output_dir=output_dir,
+                                    force_rewrite=True,
+                                    save_json=True,
+                                    load_json=True,
+                                    supp_tags=supp_tags,
+                                    nb_threads=1, verbose=False)
+    dicom_explorer.run_conversion()
+```
+
+### Dependencies
+
+```
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from Dicom_Reporter_Class import *
 
 def main():
     root = r'Z:\Morfeus'
-    input_dir = os.path.join(root, r'Bastien\MERIT\DICOM_explore_example')
+    input_dir = os.path.join(root, r'Bastien\DICOM\AIP_LACC')
     output_dir = os.path.join(input_dir, 'niftii')
+
     supp_tags = {
         'MammoDesc': '0055|1001'
     }
@@ -14,24 +15,11 @@ def main():
                                     output_dir=output_dir,
                                     force_rewrite=True,
                                     save_json=True,
-                                    load_json=False,
-                                    supp_tags=supp_tags,
-                                    nb_threads=32, verbose=False)
-    dicom_explorer.run_conversion()
-    print("     Elapse time {}".format(time.time() - time_start))
-
-    time_start = time.time()
-    dicom_explorer = Dicom_Reporter(input_dir=input_dir,
-                                    output_dir=output_dir,
-                                    force_rewrite=False,
-                                    save_json=True,
                                     load_json=True,
                                     supp_tags=supp_tags,
-                                    nb_threads=32, verbose=False)
+                                    nb_threads=1, verbose=False)
     dicom_explorer.run_conversion()
     print("     Elapse time {}".format(time.time() - time_start))
-    # dicom_explorer.dicom_dict
-
 
 if __name__ == '__main__':
     main()
