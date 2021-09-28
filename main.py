4ec0ab36b856f49ee3e9c7a0d6c8e4b2f7ad06a7
Rigaud <BSRigaud@mdanderson.org>
2021-09-28 15:30:35 -0500

    add template for rt or mammography application

--- main.py
+++ main.py
@@ -2,27 +2,32 @@ from Dicom_Reporter_Class import Dicom_Reporter
 
 
 def main():
-    input_dir = r'Z:\Morfeus\Bastien\DICOM\EBRT_test'
-    output_dir = r'Z:\Morfeus\Bastien\DICOM\EBRT_test\nifti'
+    input_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_explore_example'
+    output_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_explore_example\nifti'
 
     supp_tags = {
     }
 
     contour_names = []
-    contour_association = {
-    }
+    contour_association = {}
+
+    rt_template = {'image_series_id': False,
+                   'study_desc_name': True,
+                   'merge_study_serie_desc': True}
+    
+    mg_template = {'image_series_id': True,
+                   'study_desc_name': False,
+                   'merge_study_serie_desc': False}
+
     dicom_explorer = Dicom_Reporter(input_dir=input_dir,
                                     output_dir=output_dir,
                                     contour_names=contour_names,
                                     contour_association=contour_association,
                                     force_rewrite=True,
-                                    image_series_id=False,
-                                    study_desc_name=True,
-                                    merge_study_serie_desc=True,
                                     save_json=True,
                                     load_json=True,
                                     supp_tags=supp_tags,
-                                    nb_threads=24, verbose=True)
+                                    nb_threads=24, verbose=True, **mg_template)
     dicom_explorer.run_conversion()
 
 
