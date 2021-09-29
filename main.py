bf82c9ea9d4ae9f628341cf3771c9581749dc265
Rigaud <BSRigaud@mdanderson.org>
2021-09-29 11:53:27 -0500

    change path

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from Dicom_Reporter_Class import Dicom_Reporter
 
 
 def main():
-    input_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_explore_example'
-    output_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_explore_example\nifti'
+    # input_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_study_v1\Consolidated\DATA'
+    # output_dir = r'Z:\Morfeus\Bastien\MERIT\nifti_consolidated'
+
+    input_dir = r'/workspace/Morfeus/Bastien/MERIT/DICOM_study_v1/Consolidated/DATA'
+    output_dir = r'/workspace/Morfeus/Bastien/MERIT/DICOM_study_v1/nifti_consolidated'
     supp_tags = {}
     contour_names = []
     contour_association = {}
