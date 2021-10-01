0a449a6b34571088bd50256016f44815f3508a92
Rigaud <BSRigaud@mdanderson.org>
2021-10-01 16:50:56 -0500

    test write on dgx

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from Dicom_Reporter_Class import Dicom_Reporter
 def main():
     # input_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_TEST'
     # output_dir = r'Z:\Morfeus\Bastien\MERIT\DICOM_TEST\nifti'
-    #
-    input_dir = r'/workspace/Morfeus/Bastien/MERIT/DICOM_study_v1/Consolidated/DATA'
-    output_dir = r'/workspace/Morfeus/Bastien/MERIT/nifti_consolidated'
+
+    input_dir = r'/workspace/Morfeus/Bastien/MERIT/DICOM_TEST'
+    output_dir = r'/workspace/Morfeus/Bastien/MERIT/nifti'
+
+    # input_dir = r'/workspace/Morfeus/Bastien/MERIT/DICOM_study_v1/Consolidated/DATA'
+    # output_dir = r'/workspace/Morfeus/Bastien/MERIT/nifti_consolidated'
     supp_tags = {}
     contour_names = []
     contour_association = {}
